cf1ddb9df13c1ffdb9cdb4f5c9a53a0cc2b2ac8c
Oleh <oleh.hrechukh@eleks.com>
2023-03-22 15:34:37 +0100

    upd version

--- src/Football.Bot.AzureFunction/Functions/TelegramFunctions.cs
+++ src/Football.Bot.AzureFunction/Functions/TelegramFunctions.cs
@@ -57,7 +57,7 @@ public class TelegramFunctions
         [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)]
         HttpRequest req, ILogger log)
     {
-        return new OkObjectResult(new {status = "ok", v = 1});
+        return new OkObjectResult(new {status = "ok", v = 1.1});
     }
 
     private async Task HandleUpdate(Update update)
